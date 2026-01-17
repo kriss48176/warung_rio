5fcf755571689b570f0cbc7258c2f8219d886ec0
kriss48176 <kristoferhenry82@gmail.com>
2026-01-18 01:57:16 +0700

    Fix: Force clear config cache on Vercel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,7 @@
 <?php
+
+// Jalankan pembersihan cache secara otomatis setiap kali dipanggil di Vercel
+exec('php artisan config:clear');
+exec('php artisan view:clear');
+
 require __DIR__ . '/../public/index.php';
