cc5cca2493aad96931722f6d6ed812aa95f53800
kriss48176 <kristoferhenry82@gmail.com>
2026-01-18 04:31:02 +0700

    deploy laravel

--- api/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Jalankan pembersihan cache secara otomatis setiap kali dipanggil di Vercel
-exec('php artisan config:clear');
-exec('php artisan view:clear');
-
-require __DIR__ . '/../public/index.php';
